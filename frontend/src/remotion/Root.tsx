5a031eb6c0c261d1321a80b328556905c48d3b22
Lord Neo Barnachea <lordneobarnachea@gmail.com>
2026-03-08 23:39:15 +0800

    Refactor: Remove dynamic duration and FPS input properties and calculation from Remotion Composition.

--- frontend/src/remotion/Root.tsx
+++ frontend/src/remotion/Root.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import { Composition, registerRoot } from 'remotion';
 import { DynamicComposition } from './DynamicComposition';
 
-interface InputProps {
-  code: string;
-  durationInFrames: number;
-  fps: number;
-}
-
 function RemotionRoot() {
   return (
     <Composition
@@ -17,15 +11,7 @@ function RemotionRoot() {
       fps={30}
       width={1920}
       height={1080}
-      defaultProps={{
-        code: '',
-        durationInFrames: 180,
-        fps: 30,
-      }}
-      calculateMetadata={async ({ props }: { props: InputProps }) => ({
-        durationInFrames: props.durationInFrames || 180,
-        fps: props.fps || 30,
-      })}
+      defaultProps={{ code: '' }}
     />
   );
 }
